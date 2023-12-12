9651bf084c33d321588f93c4f7e5455525a1763c
Alex Dopico <alex.dopico@gmail.com>
2023-12-12 20:37:16 +0100

    added AnnalynInfiltration

---
+++ exercism-java/AnnalynInfiltration/src/main/java/AnnalynInfiltration.java
@@ -0,0 +1,16 @@
+/**
+ * ISTRUCTIONS
+ * 1. Check if a fast attack can be made:
+ * A fast attack can be made when the knight is sleeping.
+ * 2. Check if the group can be spied upon:
+ * The group can be spied if one of them is awake.
+ * 3. Check if the prisoner can be signaled:
+ * The prisoner can be signaled if prisoner is awake
+ * and the archer is sleeping.
+ * 4. Check if the prisoner can be freed:
+ * If Annalyn has her pet and the archer is asleep OR
+ * if archer and knight are both sleeping.
+ */
+
+public class AnnalynInfiltration {
+}
